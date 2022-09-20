e77b445252bb79705d340f1435b246249f53ab3c
Dan Clark <danclark@redhat.com>
2022-09-20 00:24:05 -0400

    Updates to quay packer

--- packer/bootstrap.sh
+++ packer/bootstrap.sh
@@ -17,7 +17,7 @@ sudo dnf -y install ansible
 # Install ansibl roles
 ansible-galaxy role install redhatofficial.rhel8_stig
 
-ansible-playbook /tmp/harden_quay.yaml
+#ansible-playbook /tmp/harden_quay.yaml
 
 # Pull OCP Dependencies
 curl -O -L "https://developers.redhat.com/content-gateway/file/pub/openshift-v4/clients/mirror-registry/1.2.6/mirror-registry.tar.gz"
@@ -28,14 +28,15 @@ curl -O -L "https://mirror.openshift.com/pub/openshift-v4/x86_64/clients/ocp/lat
 tar -xzf openshift-install-linux.tar.gz
 tar -xzf openshift-client-linux.tar.gz
 tar -xzf mirror-registry.tar.gz
+tar -xzf oc-mirror.tar.gz
 rm -f openshift-install-linux.tar.gz  
 rm -f openshift-client-linux.tar.gz
 rm -f README.md
-sudo mv -f openshift-install oc kubectl /usr/local/bin/
+sudo mv -f openshift-install oc kubectl oc-mirror /usr/local/bin/
 pushd /usr/local/bin
-sudo chown root.root openshift-install oc kubectl
-sudo chmod 0755 openshift-install oc kubectl
-sudo restorecon -v openshift-install oc kubectl
+sudo chown root.root openshift-install oc kubectl oc-mirror
+sudo chmod 0755 openshift-install oc kubectl oc-mirror
+sudo restorecon -v openshift-install oc kubectl oc-mirror
 popd
 
 sudo dnf -y install podman firewalld
