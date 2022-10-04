3af176868f7c6704cc72b3ce2b3aadc290f29b15
Dan Clark <danclark@redhat.com>
2022-10-03 20:34:42 -0400

    Add option to use different regions

--- packer/build.sh
+++ packer/build.sh
@@ -4,30 +4,43 @@ export PACKER_TEMPLATE="aws-rhel8-quay.json"
 
 export PULL_SECRET="/home/danclark/pull-secret.txt"
 
+# Use zone c for builds. Change to a different zone for your region if needed
+AWS_ZONE="c"
+
 if [ -z $AWS_ACCESS_KEY_ID ];
 then
   echo "AWS_ACCESS_KEY_ID Required"
   exit 1
 fi
+
 if [ -z $AWS_SECRET_ACCESS_KEY ];
 then
   echo "AWS_SECRET_ACCESS_KEY Required"
   exit 1
 fi
 
+if [ -z $AWS_DEFAULT_REGION ];
+then
+  echo "AWS_DEFAULT_REGION Required"
+  exit 1
+fi
+
+# Get default VPC ID
 export DEFAULT_VPC_ID=$(aws ec2 describe-vpcs \
   --query 'Vpcs[?IsDefault == `true`].VpcId' \
   --output text) 
 
+# Get subnet ID for az ${AWS_ZONE} in region ${AWS_DEFAULT_REGION}
 export SUBNET_ID=$(aws ec2 describe-subnets --filters "Name=vpc-id,Values=${DEFAULT_VPC_ID}" \
-  --query 'Subnets[?AvailabilityZone == `us-east-1c`].SubnetId' \
+  --query "Subnets[?AvailabilityZone == '${AWS_DEFAULT_REGION}${AWS_ZONE}'].SubnetId" \
   --output text)
 
 export SOURCE_AMI=$(aws ec2 describe-images --owners 309956199498 --region us-east-1 \
   --output text --query 'Images[*].[ImageId]' \
   --filters "Name=name,Values=RHEL-8.6?*HVM-*Hourly*" Name=architecture,Values=x86_64 | sort -r)
 
-ansible-galaxy role install redhatofficial.rhel8_stig
+# Not an ansible-local provisioner anymore.
+#ansible-galaxy role install redhatofficial.rhel8_stig
 
 packer build ${PACKER_TEMPLATE} | tee packer.log
 
