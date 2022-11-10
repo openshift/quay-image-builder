cfa2f8a96608a18cffa005119837d92dfb6b1dc9
Dan Clark <danclark@redhat.com>
2022-11-10 07:21:27 -0500

    Script to extract the ccoctl tool from the openshift release image

---
+++ get_ccoctl.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -xe
+
+RELEASE_IMAGE=$(openshift-install version | awk '/release image/ {print $3}')
+
+CCO_IMAGE=$(oc adm release info --image-for='cloud-credential-operator' $RELEASE_IMAGE)
+
+oc image extract $CCO_IMAGE --file="/usr/bin/ccoctl" -a /tmp/pull-secret.txt
+
+sudo mv ccoctl /usr/local/bin
+sudo chown root.root /usr/local/bin/ccoctl
+sudo chmod 0755 /usr/local/bin
+sudo restorecon -v /usr/local/bin
+
+exit 0
